96475afc33838a956c0d96be5a587101c1a57f60
Henrry Bourgeot <henrrybrgt@gmail.com>
2024-03-01 23:35:42 -0400

    Add frontend and dependencies

--- cmd/goxcel/main.go
+++ cmd/goxcel/main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/hbourgeot/goxcel/excel"
 	"github.com/hbourgeot/goxcel/utils"
 )
@@ -35,6 +36,8 @@ func NewRouter() *chi.Mux {
 }
 
 func (app *App) Mount() {
+	app.Router.Use(middleware.Recoverer)
+	app.Router.Use(cors.Handler(cors.Options{AllowedOrigins: []string{"*"}, AllowCredentials: true}))
 	app.Router.Use(middleware.Logger)
 
 	app.Router.Post("/initGoxcel/{user}", app.initGoxcel)
